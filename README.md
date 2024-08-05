f51c2513d4735da4f023118847164edcf641330f
unknown <yamate.konoha@gmail.com>
2024-08-05 14:17:54 -0400

    fix ipfs

--- README.md
+++ README.md
@@ -2,18 +2,46 @@
 
 This software "sunrise-data" is a program to publish and retrieve the BLOB data from off chain storage like IPFS, Arweave and so on.
 
+## Run sunrise chain node
+
+
+## Run Local IPFS Node
+sunrise-data uses IPFS protocol to upload metadata and shard to IPFS.
+### 1. Download and extract ipfs node binary
+We can download official prebuilt binaries from https://dist.ipfs.tech#kubo and Extract 'kubo_v0.29.0_linux-amd64.tar.gz" after download.
+### 2. Install IPFS
+Run command as the follows.
+```bash
+$ cd kubo_v0.29.0_linux-amd64/kubo
+$ sudo ./install.sh
+```
+To check ipfs has been installed.
+```bash
+$ ipfs version
+ipfs version 0.29.0
+```
+### 3. Initialize IPFS
+```bash
+$ ipfs init --profile=lowpower
+```
+### 4. Run IPFS as daemon
+```bash
+$ ipfs daemon
+```
+You can visit http://localhost:8080/ipfs to check runing IPFS RPC.
+
 ## Store project
 This project need to store in directory stored "cau-sunrise" codebase
 ```bash
-> ls
+$ ls
   cau-sunrise
   cau-sunrise-data
 ```
 
 ## Run API Service
 ```sh
-go mod tidy
-go run .
+$ go mod tidy
+$ go run .
 ```
 
 ## API Endpoint
@@ -24,7 +52,8 @@ Request JSON:
 ```protobuf
 {
     "blob": "Base64 Encoded string",
-    "shard_count_half": number
+    "shard_count_half": number,
+    "protocol": "ipfs" or "arweave"
 }
 ```
 Response JSON:
@@ -34,7 +63,7 @@ Response JSON:
 }
 ```
 
-### 2. GET http://localhost:8080/api/uploaded_data?metadata_uri=[metadata_uri]
+### 2. GET http://localhost:8080/api/uploaded_data?metadata_uri=[metadata_uri]&indices=1,2,3
 
 Response:
 ```protobuf
@@ -45,7 +74,54 @@ Response:
         "uri1",
         "uri2",
         ...
+    ],
+    shard_hashes:[
+        "base64 encoded shard for index1",
+        "base64 encoded shard for index2",
+        "base64 encoded shard for index3",
+        ...
     ]
 }
 ```
 
+### 3. Issue on API
+In case that  error occurs on API service, Endpoint returns HTTP 400 code and error msg.
+
+## Testing Example for API Endpoint
+### 1. Publsh
+```protobuf
+POST http://localhost:8000/api/publish
+Request
+{
+    "blob": "MTIzNDU2Nzg5MDEyMzQ1Njc4OTA",   // "12345678901234567890"
+    "shard_count_half": 3,
+    "protocol": "ipfs" // or "arweave"
+}
+
+Response
+{
+    metadata_uri: "/ipfs/QmPdJ4GtFRvpkbsn47d1HbEioSYtSvgAYDkq5KsL5xUb1C"
+}
+```
+
+### 2. Uploaded Data API
+```protobuf
+GET http://localhost:8000/api/uploaded_data?metadata_uri=/ipfs/QmPdJ4GtFRvpkbsn47d1HbEioSYtSvgAYDkq5KsL5xUb1C&indices=1,2,3
+
+{
+    "shard_size":7,
+    "shard_count":6,
+    "shard_uris":[
+        "/ipfs/QmYbgKse7s4S1qSrz139zsPECYSu9HbHuz9TBy7ZDEKi54",
+        "/ipfs/QmWGhZL3maoUPbaYNauhq4BLL33xZdrf9Bi7xHUMFgtnV7",
+        "/ipfs/QmapUiNguJpqfuWdxtUJ1GPp5264yCLN5aMJqgWJvxdaEu",
+        "/ipfs/QmXLtGEkGVcRZukdaftW3M979SPWDaZidt6EpjkEk4SjCv",
+        "/ipfs/QmQzrZhSG3hAfwfJinMjiwC66MnJV6LxaVtLNKCjWaRdmj",
+        "/ipfs/Qmd2tYLCM7YecjoLA9ppJNPRgQnshVfdoiPwnux3WiyS2H"
+    ],
+    "shard_hashes":[
+        "JvpetAD7cXIa6zMnWYOyOCfYD+g68xbHBVU5CEKz9OI=",
+        "DKLinTzcoegAW/1rEIfBswH0ZXu6+W0V01PZb83Xmzg=",
+        "A0+kyUqS08YRt34emU3OISrcCOWn3z7kCkBzftiKqog="
+    ]
+}
--- api/api.go
+++ api/api.go
@@ -3,16 +3,21 @@ package api
 import (
 	"bytes"
 	"context"
+	"crypto/sha256"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
+	"strconv"
+	"strings"
 
 	"net/http"
 
 	"encoding/base64"
 
 	native_mimc "github.com/consensys/gnark-crypto/ecc/bn254/fr/mimc"
+	"github.com/everFinance/goar"
+	goar_types "github.com/everFinance/goar/types"
 	"github.com/gorilla/mux"
 	"github.com/ipfs/boxo/files"
 	"github.com/ipfs/boxo/path"
@@ -23,15 +28,22 @@ import (
 	"github.com/sunriselayer/sunrise/x/da/types"
 )
 
+const (
+	IPFS_PROTOCOL    = "ipfs"
+	ARWEAVE_PROTOCOL = "arweave"
+)
+
 type UploadedDataResponse struct {
-	ShardSize  uint64   `json:"shard_size"`
-	ShardCount uint64   `json:"shard_count"`
-	ShardUris  []string `json:"shard_uris"`
+	ShardSize   uint64   `json:"shard_size"`
+	ShardCount  uint64   `json:"shard_count"`
+	ShardUris   []string `json:"shard_uris"`
+	ShardHashes []string `json:"shard_hashes"`
 }
 
 type PublishRequest struct {
 	Blob           string `json:"blob"`
 	ShardCountHalf int    `json:"shard_count_half"`
+	Protocol       string `json:"protocol"`
 }
 
 type PublishResponse struct {
@@ -46,6 +58,7 @@ func Handle() {
 		w.Write([]byte("Welcome to cau-sunrise-data API"))
 	}).Methods("GET")
 	r.HandleFunc("/api/publish", func(w http.ResponseWriter, r *http.Request) {
+
 		var req PublishRequest
 		err := json.NewDecoder(r.Body).Decode(&req)
 		if err != nil {
@@ -57,9 +70,24 @@ func Handle() {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
+		protocol := IPFS_PROTOCOL
+		if req.Protocol == IPFS_PROTOCOL {
+			protocol = IPFS_PROTOCOL
+		} else if req.Protocol == ARWEAVE_PROTOCOL {
+			protocol = ARWEAVE_PROTOCOL
+		} else {
+			http.Error(w, "Invalid Protocol", http.StatusBadRequest)
+			return
+		}
+
+		recoveredDataHash, err := hashSha256(blobBytes)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
 		shardSize, shardCount, shards := erasurecoding.ErasureCode(blobBytes, req.ShardCountHalf)
-		shardUris, err := getShardUris(shards)
+		shardUris, err := getShardUris(shards, protocol)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
@@ -76,11 +104,21 @@ func Handle() {
 		}
 
 		//upload ipfs
-		metadataUri, err := uploadIpfs(metadataBytes)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
+		metadataUri := ""
+		if protocol == IPFS_PROTOCOL {
+			metadataUri, err = uploadToIpfs(metadataBytes)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+		} else {
+			metadataUri, err = uploadToArweave(metadataBytes)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
 		}
+
 		ctx := context.Background()
 		nodeClient, err := cosmosclient.New(ctx, cosmosclient.WithAddressPrefix(SUNRISE_ADDR_PRIFIX))
 		if err != nil {
@@ -105,7 +143,7 @@ func Handle() {
 		// Define a message to create a post
 		msg := &types.MsgPublishData{
 			Sender:            addr,
-			RecoveredDataHash: hashMimc(blobBytes),
+			RecoveredDataHash: recoveredDataHash,
 			MetadataUri:       metadataUri,
 			ShardDoubleHashes: byteSlicesToDoubleHashes(shards),
 		}
@@ -126,11 +164,13 @@ func Handle() {
 
 	r.HandleFunc("/api/uploaded_data", func(w http.ResponseWriter, r *http.Request) {
 		metadataUri := r.URL.Query().Get("metadata_uri")
+		indices := r.URL.Query().Get("indices")
+		indicesList := strings.Split(indices, ",")
 		if metadataUri == "" {
 			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
 			return
 		}
-		metadataBytes, err := getDataFromIpfs(metadataUri)
+		metadataBytes, err := getDataFromIpfsOrArweave(metadataUri)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
@@ -142,19 +182,37 @@ func Handle() {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
+		shardHashes := []string{}
+		fmt.Println(len(indicesList))
+		for _, index := range indicesList {
+			iIndex, err := strconv.Atoi(index)
+			if err != nil {
+				continue
+			}
+			if iIndex < len(metadata.ShardUris) {
+				shardUri := metadata.ShardUris[iIndex]
+				shardData, err := getDataFromIpfsOrArweave(shardUri)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusBadRequest)
+					return
+				}
+				shardHashes = append(shardHashes, base64.StdEncoding.EncodeToString(hashMimc(shardData)))
+			}
+		}
 
 		res := UploadedDataResponse{
-			ShardSize:  metadata.ShardSize,
-			ShardCount: metadata.ShardCount,
-			ShardUris:  metadata.ShardUris,
+			ShardSize:   metadata.ShardSize,
+			ShardCount:  metadata.ShardCount,
+			ShardUris:   metadata.ShardUris,
+			ShardHashes: shardHashes,
 		}
 
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(res)
 
 	}).Methods("GET")
-	fmt.Println("Running on localhost:8080")
-	http.ListenAndServe(":8080", r)
+	fmt.Println("Running Publisher API on localhost:8000")
+	http.ListenAndServe(":8000", r)
 
 }
 
@@ -177,20 +235,37 @@ func doubleHashMimc(data []byte) []byte {
 	return hashMimc(hashData)
 }
 
+func hashSha256(data []byte) ([]byte, error) {
+	h := sha256.New()
+	_, err := h.Write(data)
+	if err != nil {
+		return nil, err
+	}
+	return h.Sum(nil), nil
+}
+
 // upload shards to ipfs
-func getShardUris(inputData [][]byte) ([]string, error) {
+func getShardUris(inputData [][]byte, protocol string) ([]string, error) {
 	var shardUris []string
 	for _, data := range inputData {
-		shardUri, err := uploadIpfs(data)
-		if err != nil {
-			return nil, err
+		if protocol == IPFS_PROTOCOL {
+			shardUri, err := uploadToIpfs(data)
+			if err != nil {
+				return nil, err
+			}
+			shardUris = append(shardUris, shardUri)
+		} else {
+			shardUri, err := uploadToArweave(data)
+			if err != nil {
+				return nil, err
+			}
+			shardUris = append(shardUris, shardUri)
 		}
-		shardUris = append(shardUris, shardUri)
 	}
 	return shardUris, nil
 }
 
-func uploadIpfs(inputData []byte) (string, error) {
+func uploadToIpfs(inputData []byte) (string, error) {
 	node, err := rpc.NewLocalApi()
 	if err != nil {
 		return "", err
@@ -204,24 +279,44 @@ func uploadIpfs(inputData []byte) (string, error) {
 	}
 	return cidFile.String(), nil
 }
+func uploadToArweave(inputData []byte) (string, error) {
+	wallet, err := goar.NewWalletFromPath("../keyfile.json", "https://arweave.net")
+	if err != nil {
+		return "", err
+	}
+	tx, err := wallet.SendData(
+		inputData, // Data bytes
+		[]goar_types.Tag{},
+	)
+	if err != nil {
+		return "", err
+	}
+	return tx.ID, nil
+}
 
-func getDataFromIpfs(uri string) ([]byte, error) {
-	node, err := rpc.NewLocalApi()
-	if err != nil {
-		return nil, err
+func getDataFromIpfsOrArweave(uri string) ([]byte, error) {
+	if uri[:6] == "/ipfs/" { //ipfs
+		node, err := rpc.NewLocalApi()
+		if err != nil {
+			return nil, err
+		}
+		ctx := context.Background()
+		cidData, err := cid.Parse(uri)
+		if err != nil {
+			return nil, err
+		}
+		data, err := node.Unixfs().Get(ctx, path.FromCid(cidData))
+		if err != nil {
+			return nil, err
+		}
+		r, ok := data.(files.File)
+		if !ok {
+			return nil, errors.New("incorrect type from Unixfs().Get()")
+		}
+		return io.ReadAll(r)
+	} else { //arweave
+		arweaveClient := goar.NewClient("https://arweave.net")
+		return arweaveClient.GetTransactionData(uri)
 	}
-	ctx := context.Background()
-	cidData, err := cid.Parse(uri)
-	if err != nil {
-		return nil, err
-	}
-	data, err := node.Unixfs().Get(ctx, path.FromCid(cidData))
-	if err != nil {
-		return nil, err
-	}
-	r, ok := data.(files.File)
-	if !ok {
-		return nil, errors.New("incorrect type from Unixfs().Get()")
-	}
-	return io.ReadAll(r)
+
 }
