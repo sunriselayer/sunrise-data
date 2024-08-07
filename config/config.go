2a649e60939f68964f7c3aeacf0f586b92a37e1b
jununifi <jun.ununifi@outlook.com>
2024-08-07 11:03:46 +0800

    add more config and params

--- config/config.go
+++ config/config.go
@@ -1,6 +1,10 @@
 package config
 
 const (
-	API_PORT uint = 8000
-	IPFS_API_URL      = ""
+	API_PORT          uint = 8000
+	IPFS_API_URL           = ""
+	PUBLISHER_ACCOUNT      = "validator"
+	SUNRISE_HOME_DIR       = "/Users/admin/.sunrise"
+	FEES                   = "10000uvrise"
+	KEYRING_BACKEND        = "test"
 )
